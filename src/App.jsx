7bf4efa64bca4b750286cc89e647d4e85dda134f
phoenixfire77777 <phoenixfire77777@gmail.com>
2023-07-27 16:29:46 -0400

    added authenticate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import SignUpForm from "./assets/components/SignUpForm";
+import Authenticate from "./assets/components/Authenticate";
+import { useState } from "react";
+export default function App() {
+  const [token, setToken] = useState(null);
 
-
-function App() {
-  
   return (
     <>
-      
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
-
-export default App
